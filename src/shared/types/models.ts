c83f3cc3462a0f42d3e321fbdfa3a748f78e0a61
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-12 19:58:23 +0100

    feat: auth types

---
+++ src/shared/types/models.ts
@@ -0,0 +1,12 @@
+export type registerType = {
+  fullname: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  role: string;
+};
+
+export type loginType = {
+  email: string;
+  password: string;
+};
